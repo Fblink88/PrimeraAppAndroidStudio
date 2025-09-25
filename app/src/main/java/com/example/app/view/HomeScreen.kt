b3e022d73611a638705da401b5920c60277ba438
Sebas <sebas.jaral@profesor.duoc.cl>
2025-09-25 08:18:08 -0300

    Primer commit

---
+++ app/src/main/java/com/example/app/view/HomeScreen.kt
@@ -0,0 +1,73 @@
+package com.example.app.view
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.app.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun homeScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text("Mi Aplicación en Kotlin") })
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "Bienvenid@!",
+                fontWeight = FontWeight.Bold
+            )
+            Button(onClick = {/*Acción para que me lleve a otra ventana que muestre una imagen */},
+                colors = ButtonDefaults.buttonColors(
+                    Color.Red,
+                    Color.White
+                )
+            ) {
+                Text("Presióname")
+
+
+            }
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                "Logo App",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp),
+                contentScale = ContentScale.Fit
+
+            )
+        }
+
+    }
+}
+
+@Preview (showBackground = true)
+@Composable
+fun homeScreenPreview(){
+    homeScreen()
+}
+
